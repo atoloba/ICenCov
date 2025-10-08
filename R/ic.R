cf4ad0a9ab371dc366b2efca8df31148e1b29044
Andrea Toloba <andrea.toloba@upc.edu>
2025-10-08 22:59:18 +0200

    Make ic() exportable

--- R/ic.R
+++ R/ic.R
@@ -1,13 +1,13 @@
 #' Construct an interval-censored covariate for formulas
 #'
 #' Helper used inside model formulas to bundle left/right endpoints of an
-#' interval-censored covariate. 
+#' interval-censored covariate.
 #'
 #' @param zl Numeric vector of left endpoints.
 #' @param zr Numeric vector of right endpoints.
 #' @param name (optional) Name of the variable; defaults to \emph{"z"}.
-#' 
-#' @keywords internal
+#'
+#' @export
 #' @noRd
 ic <- function(zl, zr, name = "z"){
   out <- cbind(zl,zr)
